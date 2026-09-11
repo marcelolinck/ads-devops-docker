fa7faaf6cc14ae2cfb4d172425a179e8a98a5b56
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2026-09-10 21:33:08 -0400

    feat: ajustado docker para uso do compose.

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const app = express();
 const pool = mysql.createPool({
   host: 'mysql-db',      // nome do container — é isso que a rede resolve
   user: 'root',
-  port: 3306,            // porta interna do container MySQL (3307 é só o mapeamento externo)
-  password: 'senha123',
-  database: 'loja',
+  port: process.env.MYSQL_DB_PORTA,            // porta interna do container MySQL (3307 é só o mapeamento externo)
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
 });
 
 app.get('/categorias', async (req, res) => {
   const [rows] = await pool.query('SELECT * FROM categorias');
-  res.json(rows);
+  res.json(rows.length);
 });
 
 app.listen(3000, "0.0.0.0", () => {
@@ -25,5 +25,5 @@ app.get('/produtos', async (req, res) => {
     FROM produtos p
     JOIN categorias c ON p.categoria_id = c.id
   `);
-  res.json(rows);
+  res.json(rows.length);
 });
